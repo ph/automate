8df7ecf0288502e15a6284a6b3ba86e93fa32f3a
Pier-Hugues Pellerin <phpellerin@gmail.com>
2025-03-15 19:21:27 -0400

    feat: add nonguix substitutes servers
    They provide the firefox binaries..

--- packages/common.scm
+++ packages/common.scm
@@ -120,6 +120,14 @@
 
 (define %my-desktop-services
   (modify-services %base-services
+		   (guix-service-type config => (guix-configuration
+						 (inherit config)
+						 (substitute-urls
+						  (append (list "https://substitutes.nonguix.org")
+							  %default-substitute-urls))
+						 (authorized-keys
+						  (append (list (local-file "../files/substitutes_keys/nonguix-signing-key.pub"))
+							  %default-authorized-guix-keys))))
 		   (delete console-font-service-type)))
 
 (define %my-system-services
