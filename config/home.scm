db5990cfd4ed489dc9d7ecc18dbbf09a1135bec5
Pier-Hugues Pellerin <phpellerin@gmail.com>
2025-04-01 12:01:00 -0400

    add fish-foreign-env set up vi keybind

--- config/home.scm
+++ config/home.scm
@@ -504,10 +504,13 @@
 			direnv "/bin/direnv hook fish | source")
 		       (mixed-text-file
 			"disable-fish-greetings" "set -U fish_greeting")
+		       (mixed-text-file
+			"enable-foreign-fish-env"
+			"set fish_function_path $fish_function_path $HOME/.guix-profile/share/fish/functions")
 		       (plain-file
 			"fish-hydro-config" "\
 set -g hydro_color_pwd \"brcyan\"
+set -g fish_key_bindings fish_vi_key_bindings
 set -g fish_term24bit 1")
 		       ))))
-    (service home-fish-hydro-service-type)
     %base-home-services)))
