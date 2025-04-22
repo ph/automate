0e63722da02d5ff8e002bfac6b5069000486ed08
ph <phpellerin@gmail.com>
2025-04-22 10:03:12 -0400

    move forward with the initial changes in documents structures

--- flake.nix
+++ flake.nix
@@ -5,8 +5,10 @@
     # NixOS official package source, using the nixos-24.11 branch here
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-    disko.url = "github:nix-community/disko/latest";
-    disko.inputs.nixpkgs.follows = "nixpkgs";
+    disko = {
+      url = "github:nix-community/disko/latest";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     catppuccin.url = "github:catppuccin/nix";
 
@@ -19,42 +21,53 @@
       url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-   # stylix = {
-    #   url = "github:danth/stylix";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    # }; 
-    # arkenfox = {
-    #   url = "github:dwarfmaster/arkenfox-nixos";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    # };
   };
 
   outputs = { self, nixpkgs, disko, home-manager, catppuccin, firefox-addons, ... }@inputs: {
+    nixosConfigurations = builtins.listToAttrs(
+      map(host: {
+        name = host;
+        value = nixpkgs.lib.nixosSystem {
+          modules = [ ./hosts/${host} ];
+        };
+      }) (builtins.attrNames (builtins.readDir ./hosts ))
+    );
 
-    nixosConfigurations.babayaga = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        catppuccin.nixosModules.catppuccin
+    # nixosConfigurations = builtins.listToAttrs(
+    #   map (host: {
+    #     name = host;
+    #     value = nixpkgs.lib.nixosSystem {
+    #       specialArgs {
+    #         inherit inputs outputs libl;
+    #       };
+    #       modules = [ ./hosts/${host} ];
+    #     };
+    #   }) (builins.attrName (builtins.readDir ./hosts ))
+    # );
+    # nixosConfigurations.babayaga = nixpkgs.lib.nixosSystem {
+    #   system = "x86_64-linux";
+    #   modules = [
+    #     catppuccin.nixosModules.catppuccin
 
-        disko.nixosModules.disko
-        ./configuration.nix
+    #     disko.nixosModules.disko
+    #     ./configuration.nix
 
-        home-manager.nixosModules.home-manager {
-          home-manager.backupFileExtension = "backup";
-          home-manager.extraSpecialArgs = { inherit inputs; };
-        }
-        {
-          home-manager.users.ph = {
-            nixpkgs.overlays = [ firefox-addons.overlays.default ];
+    #     home-manager.nixosModules.home-manager {
+    #       home-manager.backupFileExtension = "backup";
+    #       home-manager.extraSpecialArgs = { inherit inputs; };
+    #     }
+    #     {
+    #       home-manager.users.ph = {
+    #         nixpkgs.overlays = [ firefox-addons.overlays.default ];
 
-            imports = [
-              ./home.nix
-              catppuccin.homeModules.catppuccin
-            ];
-          };
-        }
-      ];
-    };
+    #         imports = [
+    #           ./home.nix
+    #           catppuccin.homeModules.catppuccin
+    #         ];
+    #       };
+    #     }
+    #   ];
+    # };
+
   };
 }
