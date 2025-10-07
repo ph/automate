63799f356ff7abbd71571fb65a103e57ba95dbc1
Pier-Hugues Pellerin <phpellerin@gmail.com>
2025-10-07 11:17:46 -0400

    fix: add license headers
    Add missing headers for:
    
      - config/azzael.scm
      - dest/remove-boot.scm
      - scripts/azzael.sh

--- dist/remote-boot.scm
+++ dist/remote-boot.scm
@@ -1,3 +1,6 @@
+;;; SPDX-FileCopyrightText: 2025 Pier-Hugues Pellerin <ph@heykimo.com>
+;;;
+;;; SPDX-License-Identifier: GPL-3.0-or-later
 (use-modules (gnu services avahi)
 	     (gnu services networking)
 	     (gnu services ssh)
--- scripts/azzael.sh
+++ scripts/azzael.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2025 2025 Pier-Hugues Pellerin <ph@heykimo.com>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
 set -e
 set -p pipefail
 set -o xtrace
