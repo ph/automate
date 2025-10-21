ddc2aa3f4f0b3cad273a80735a822d05b8ebf080
Pier-Hugues Pellerin <phpellerin@gmail.com>
2025-10-21 14:03:57 -0400

    Make make check happy

--- dist/worker-vm.scm
+++ dist/worker-vm.scm
@@ -1,3 +1,7 @@
+;;; SPDX-FileCopyrightText: 2025 2025 Pier-Hugues Pellerin <ph@heykimo.com>
+;;;
+;;; SPDX-License-Identifier: GPL-3.0-or-later
+
 (use-modules (gnu)
 	     (gnu image)
 	     (gnu system image))
