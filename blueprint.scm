01dc132ee24b68188b8473d4934edcd7f7ec98f3
Pier-Hugues Pellerin <phpellerin@gmail.com>
2026-04-06 17:37:33 -0400

    arguments works

--- blueprint.scm
+++ blueprint.scm
@@ -13,13 +13,16 @@
  (blue types configuration)
  (blue types variable)
  (blue utils strings)
+ (blue subprocess)
  (srfi srfi-1)
+ (ice-9 match)
  (oop goops))
 
 (define-preferences
   (blue.stencils.tangle:emacs
-   #:default (const (delay (or (getenv "EMACS") (find-binary "emacs"))))
-   #:valid-values string?
+   #:default (const (delay (list (or (getenv "EMACS")
+				     (find-binary "emacs")))))
+   #:valid-values (list-of? string?)
    #:documentation "The binary to use for `emacs(1)`.  `emacs' if the environment is defined, otherwise
 `emacs'."))
 
@@ -28,7 +31,7 @@
    #:getter tangle-executable
    #:init-value #%~#%?EMACS
    #:init-keyword #:executable
-   #:type string?))
+   #:type (list-of? string?)))
 
 (define %tangle-configuration
   (configuration
@@ -41,29 +44,38 @@
 (define-method (ask-build-configurations (this <tangle>))
   (list %tangle-configuration))
 
+(define (run cmd)
+  (match cmd
+    ((prog . args)
+     (let ((exit-val (popen prog args)))
+       (zero? exit-val)))))
+
 (define-method (ask-build-manifest (this <tangle>) (inputs <list>) (outputs <list>))
   (make-build-manifest
    (format #f "TANGLE ~a to ~a" (string-join inputs ",") (string-join outputs ","))
    (lambda()
-     `((tangle-executable this) ,@(emacs-arguments (first inputs))))))
+
+     (run (append (tangle-executable this)
+		  (emacs-arguments (first inputs)))))))
 
 (define (emacs-arguments file)
-  `(list "--quick"
-	 "--batch"
-	 "--load" "ob-tangle"
-	 "--eval" "(setopt org-babel-load-languages '((shell . t)))"
-	 "--eval" "(setopt org-confirm-babel-evaluate nil)"
-	 "--eval" "(setopt org-id-track-globally nil)"
-	 "--eval" ,(format #f "(org-babel-tangle-file ~s)" file)))
+  (list "--quick"
+	"--batch"
+	"--load" "ob-tangle"
+	"--eval" "(setopt org-babel-load-languages '((shell . t)))"
+	"--eval" "(setopt org-confirm-babel-evaluate nil)"
+	"--eval" "(setopt org-id-track-globally nil)"
+	"--eval" (format #f "(org-babel-tangle-file ~s)" file)))
 
 (define %guix-emacs-minimal
-  "guix shell emacs-minimal -- emacs")
+  '("guix" "shell" "emacs-minimal" "--" "emacs"))
 
 (define tangle-emacs
   (tangle
    (inputs '("org-config/home.org"))
    (outputs '("home.scm"))
-   (executable %guix-emacs-minimal)))
+   ;; (executable '("emacs"))
+   ))
 
 (blueprint
  (buildables (list tangle-emacs)))
