f4eb23ac1d19b0e8d2011090ceea800036817f42
Pier-Hugues Pellerin <phpellerin@gmail.com>
2026-05-07 09:42:34 -0400

    niri: add blur

--- channels.lock.scm
+++ channels.lock.scm
@@ -2,7 +2,7 @@
        (name 'guix)
        (url "https://git.guix.gnu.org/guix.git")
        (branch "master")
-       (commit "4939bcf4bbae4d486fd7b88c7acf3f74bb571a86")
+       (commit "eea97118895d998ff2d28f8cb56386c25e942f68")
        (introduction
         (make-channel-introduction
          "9edb3f66fd807b096b48283debdcddccfea34bad"
