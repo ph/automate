44a914dbf73dcd0a7224be59514a4d533c258274
Pier-Hugues Pellerin <phpellerin@gmail.com>
2026-05-27 13:06:36 -0400

    boot from usb and openssh

--- modules/automate/image/remote-boot.scm
+++ modules/automate/image/remote-boot.scm
@@ -1,44 +1,40 @@
 (define-module (automate image remote-boot)
   #:use-module (automate profile)
-  #:use-module (gnu bootloader grub)
-  #:use-module (gnu bootloader)
   #:use-module (gnu packages linux)
   #:use-module (gnu packages)
   #:use-module (gnu services base)
-  #:use-module (gnu system file-systems)
+  #:use-module (gnu services)
+  #:use-module (gnu system install)
   #:use-module (gnu system shadow)
   #:use-module (gnu system)
-  #:use-module (gnu system install)
   #:use-module (guix gexp)
   #:use-module (nongnu packages firmware)
   #:use-module (nongnu packages linux)
-  #:use-module (nongnu system linux-initrd)
-  ;; #:use-module (gnu packages cryptsetup)
-  ;; #:use-module (gnu packages disk)
-  ;; #:use-module (gnu packages file-systems)
-  ;; #:use-module (gnu services avahi)
-  ;; #:use-module (gnu services networking)
-  ;; #:use-module (gnu services ssh)
-  )
+  #:use-module (nongnu system linux-initrd))
 
 (define %installation-os
-  (make-installation-os #:efi-only? #f))
+  (make-installation-os))
 
 (define %remote-boot-os
   (operating-system
     (inherit %installation-os)
     (kernel linux-7.0)
     (initrd microcode-initrd)
-    (firmware (list linux-firmware sof-firmware))
+    (firmware (list linux-firmware
+		    sof-firmware))
     (host-name "remote-boot")
-    (users %base-user-accounts)))
+    (label (format #f "GNU Guix remote boot connect to ~S" host-name))
+    (users %base-user-accounts)
+    (services (cons*
+	       (simple-service 'channel-file etc-service-type
+			       (list `("channels.scm" ,(local-file "../../../channels.lock.scm"))))
+	       %base-services))))
 
-;; (define +profile/remote-boot
-;;   (compose +networking/dhcp
-;; 	   +service/openssh
-;; 	   +profile/root-disabled-login-passwd
-;; 	   +profile/deployable))
+(define +profile/remote-boot
+  (compose +networking/dhcp
+	   +service/openssh
+	   +system/substitutes
+	   +profile/root-disabled-login-passwd
+	   +profile/deployable))
 
-;; (+profile/remote-boot %remote-boot-os)
-
-%remote-boot-os
+(+profile/remote-boot %remote-boot-os)
