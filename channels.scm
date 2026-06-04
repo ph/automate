981070668f2cffa92aa2b4de9bae42db69a8ecd5
Pier-Hugues Pellerin <phpellerin@gmail.com>
2026-06-04 19:22:18 -0400

    format channels.scm

--- channels.scm
+++ channels.scm
@@ -4,88 +4,88 @@
 
 (define %guix-channel
   (channel
-   (name 'guix)
+    (name 'guix)
     (url "https://git.guix.gnu.org/guix.git")
-   (branch "master")
-   (introduction
-    (make-channel-introduction
-     "9edb3f66fd807b096b48283debdcddccfea34bad"
-     (openpgp-fingerprint
-      "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
+    (branch "master")
+    (introduction
+     (make-channel-introduction
+      "9edb3f66fd807b096b48283debdcddccfea34bad"
+      (openpgp-fingerprint
+       "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
 
 (define %nonguix-channel
   (channel
-   (name 'nonguix)
-   (url "https://gitlab.com/nonguix/nonguix")
-   (branch "master")
-   (introduction
-    (make-channel-introduction
-     "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
-     (openpgp-fingerprint
-      "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5")))))
+    (name 'nonguix)
+    (url "https://gitlab.com/nonguix/nonguix")
+    (branch "master")
+    (introduction
+     (make-channel-introduction
+      "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
+      (openpgp-fingerprint
+       "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5")))))
 
 (define %rosenthal-channel
   (channel
-   (name 'rosenthal)
-   (url "https://codeberg.org/hako/rosenthal.git")
-   (branch "trunk")
-   (introduction
-    (make-channel-introduction
-     "7677db76330121a901604dfbad19077893865f35"
-     (openpgp-fingerprint
-      "13E7 6CD6 E649 C28C 3385  4DF5 5E5A A665 6149 17F7")))))
+    (name 'rosenthal)
+    (url "https://codeberg.org/hako/rosenthal.git")
+    (branch "trunk")
+    (introduction
+     (make-channel-introduction
+      "7677db76330121a901604dfbad19077893865f35"
+      (openpgp-fingerprint
+       "13E7 6CD6 E649 C28C 3385  4DF5 5E5A A665 6149 17F7")))))
 
 (define %heyk-channel
   (channel
-   (name 'heyk)
-   (url "https://github.com/ph/heyk")
-   (branch "trunk")
-   (introduction
-    (make-channel-introduction
-     "fa96f4b8e25dba3b5ea47f1365cbe9cf9ef0358c"
-     (openpgp-fingerprint
-      "AB8D 7699 282F 5F12 4949 547E C6CD 2C3D B524 1054")))))
+    (name 'heyk)
+    (url "https://github.com/ph/heyk")
+    (branch "trunk")
+    (introduction
+     (make-channel-introduction
+      "fa96f4b8e25dba3b5ea47f1365cbe9cf9ef0358c"
+      (openpgp-fingerprint
+       "AB8D 7699 282F 5F12 4949 547E C6CD 2C3D B524 1054")))))
 
 (define %guix-rusty-channel
   (channel
-   (name 'guix-rusty)
-   (url "https://github.com/ph/guix-rusty")
-   (branch "main")))
+    (name 'guix-rusty)
+    (url "https://github.com/ph/guix-rusty")
+    (branch "main")))
 
 ;; Expose BLUE - Build Language User Extensible. A generic build-system crafted
 ;; entirely in Guile.
 (define %bluebox-channel
   (channel
-   (name 'bluebox)
-   (branch "main")
-   (url "https://codeberg.org/lapislazuli/bluebox")
-   (introduction
-    (make-channel-introduction
-     "63350484aaacc362aea28fb14236019fced4050f"
-     (openpgp-fingerprint
-      "5132 3571 CEED 988F 52FC 467C 6F98 DBF3 EA7F 4B37")))))
+    (name 'bluebox)
+    (branch "main")
+    (url "https://codeberg.org/lapislazuli/bluebox")
+    (introduction
+     (make-channel-introduction
+      "63350484aaacc362aea28fb14236019fced4050f"
+      (openpgp-fingerprint
+       "5132 3571 CEED 988F 52FC 467C 6F98 DBF3 EA7F 4B37")))))
 
 (define %microvm-channel
   (channel
-   (name 'microvm)
-   (url "https://codeberg.org/pierhugues/microvm.git")
-   (branch "trunk")
-   (introduction
-    (make-channel-introduction
-     "61226b15bb6e23369810463e2dd77f691d815231"
-     (openpgp-fingerprint
-      "6068 E700 E178 0ADB 1D43  1E65 F7B8 F761 5A88 1272")))))
+    (name 'microvm)
+    (url "https://codeberg.org/pierhugues/microvm.git")
+    (branch "trunk")
+    (introduction
+     (make-channel-introduction
+      "61226b15bb6e23369810463e2dd77f691d815231"
+      (openpgp-fingerprint
+       "6068 E700 E178 0ADB 1D43  1E65 F7B8 F761 5A88 1272")))))
 
 (define %supervoid-channel
   (channel
-   (name 'supervoid)
-   (url "https://codeberg.org/pierhugues/supervoid.git")
-   (branch "trunk")
-   (introduction
-    (make-channel-introduction
-     "245db3d4ef2a29559b7dccef67afba453c9f6563"
-     (openpgp-fingerprint
-      "6068 E700 E178 0ADB 1D43  1E65 F7B8 F761 5A88 1272"))))
+    (name 'supervoid)
+    (url "https://codeberg.org/pierhugues/supervoid.git")
+    (branch "trunk")
+    (introduction
+     (make-channel-introduction
+      "245db3d4ef2a29559b7dccef67afba453c9f6563"
+      (openpgp-fingerprint
+       "6068 E700 E178 0ADB 1D43  1E65 F7B8 F761 5A88 1272"))))
   )
 
 (list %guix-channel
