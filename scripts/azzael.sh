5db1415e4d520bf932dcedf610df262f1a49707e
Pier-Hugues Pellerin <phpellerin@gmail.com>
2026-06-04 09:11:46 -0400

    changes the layout partition and add labels and nix

--- scripts/azzael.sh
+++ scripts/azzael.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# SPDX-FileCopyrightText: 2025 2025 Pier-Hugues Pellerin <ph@heykimo.com>
+# SPDX-FileCopyrightText: 2025 2026 Pier-Hugues Pellerin <ph@heykimo.com>
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
@@ -20,7 +20,7 @@ parted --script /dev/$DISK2 \
        mklabel gpt \
        mkpart primary fat32 1MiB 512MiB \
        mkpart primary linux-swap 512Mib 64GiB \
-       mkpart primary btrfs 64GiB 100% 
+       mkpart primary btrfs 64GiB 100%
 
 mkfs.btrfs -f -L root-fs --data raid1 --metadata raid1 /dev/${DISK1}3 /dev/${DISK2}3
 mount /dev/${DISK1}3 /mnt
@@ -31,6 +31,7 @@ btrfs subvol create /mnt/@
 btrfs subvol create /mnt/@home
 btrfs subvol create /mnt/@log
 btrfs subvol create /mnt/@gnu
+btrfs subvol create /mnt/@nix
 btrfs subvol create /mnt/@snapshots
 
 umount /mnt
@@ -44,18 +45,26 @@ mkdir -p /mnt/home
 mkdir -p /mnt/gnu
 mkdir -p /mnt/etc
 mkdir -p /mnt/tmp
+mkdir -p /mnt/nix
 mkdir -p /mnt/.snapshots
 mkdir -p /mnt/var/log
 
 mount -o subvol=@home /dev/${DISK1}3 /mnt/home
 mount -o subvol=@gnu /dev/${DISK1}3 /mnt/gnu
+mount -o subvol=@nix /dev/${DISK1}3 /mnt/nix
 mount -o subvol=@log /dev/${DISK1}3 /mnt/var/log
 mount -o subvol=@snapshots /dev/${DISK1}3 /mnt/.snapshots
 mount /dev/${DISK1}1 /mnt/boot/efi
+fatlabel /dev/${DISK1}1 EFIBOOT
 
 
+swaplabel -L swap-1 /dev/${DISK1}2
 mkswap /dev/${DISK1}2
 swapon -p 10 /dev/${DISK1}2
 
+swaplabel -L swap-2 /dev/${DISK2}2
 mkswap /dev/${DISK2}2
 swapon -p 5 /dev/${DISK2}2
+
+# guix install: warning: Consider running 'guix pull' followed by
+# 'guix package -u' to get up-to-date packages and security updates.
