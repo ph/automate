4c74d6a724c2178f64ba3ee016ca1a16135761cf
Pier-Hugues Pellerin <phpellerin@gmail.com>
2026-06-04 09:12:26 -0400

    configure a guix build deamon
    This is useful when booting a dedicated machine in debian rescue mode.

---
+++ scripts/guix-build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+rm -rf /var/cache/apt
+groupadd --system guixbuild
+for i in $(seq -w 1 10); do
+  useradd -g guixbuild -G guixbuild -d /var/empty -s "$(which nologin)" \
+          -c "Guix build user $i" --system "guixbuilder$i"
+done
