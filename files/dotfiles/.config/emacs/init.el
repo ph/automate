6371589b1a46439874edc117730723f2b4acd82c
Pier-Hugues Pellerin <phpellerin@gmail.com>
2026-06-23 07:57:57 -0400

    let's use doom modeline

--- files/dotfiles/.config/emacs/init.el
+++ files/dotfiles/.config/emacs/init.el
@@ -936,30 +936,11 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; UI
 ;; Display a simple mod line.
-(use-package lambda-line
-  :custom
-  ;; (lambda-line-icon-time t) ;; requires ClockFace font (see below)
-  ;; (lambda-line-clockface-update-fontset "ClockFaceRect") ;; set clock icon
-  (lambda-line-position 'bottom) ;; Set position of status-line
-  (lambda-line-abbrev t) ;; abbreviate major modes
-  (lambda-line-hspace "  ")  ;; add some cushion
-  (lambda-line-prefix t) ;; use a prefix symbol
-  (lambda-line-prefix-padding nil) ;; no extra space for prefix
-  (lambda-line-status-invert nil)  ;; no invert colors
-  (lambda-line-gui-ro-symbol  " ⨂") ;; symbols
-  (lambda-line-gui-mod-symbol " ⬤")
-  (lambda-line-gui-rw-symbol  " ◯")
-  (lambda-line-vc-symbol "")
-  (lambda-line-space-top +.50)  ;; padding on top and bottom of line
-  (lambda-line-space-bottom -.50)
-  (lambda-line-symbol-position 0.1) ;; adjust the vertical placement of symbol
-  :config
-  ;; activate lambda-line
-  (lambda-line-mode)
-  ;; set divider line in footer
-  (when (eq lambda-line-position 'top)
-    (setq-default mode-line-format (list "%_"))
-    (setq mode-line-format (list "%_"))))
+(use-package doom-modeline
+  :ensure t
+  :init
+  (doom-modeline-mode 1)
+  (setq doom-modeline-vcs-display-function #'doom-modeline-vcs-name))
 
 ;; Make the HL line more suitable for selection UI.
 (use-package lin
